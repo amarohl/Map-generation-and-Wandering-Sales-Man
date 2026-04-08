a5483a20eb42985ffa2289c3747beb078b70b336
amarohl <marohla100@gmail.com>
2026-04-08 16:37:26 -0500

    got random tile type generation in a 2D array implemented

---
+++ Main/Main.py
@@ -0,0 +1,46 @@
+import random
+import math
+
+
+Main: any
+
+class Tile:
+  def __init__(self, tileType: str):
+    self.tileType = tileType;
+    self.tileValue = 0;
+  def getTileType(self) -> str:
+    randomNum = random.randint(0, 4);
+    if randomNum == 0:
+      self.tileType = "grass";
+      self.tileValue = 0;
+    elif randomNum == 1:
+      self.tileType = "forest";
+      self.tileValue = 1;
+    elif randomNum == 2:
+      self.tileType = "hill";
+      self.tileValue = 2;
+    elif randomNum == 3:
+      self.tileType = "mountain";
+      self.tileValue = 3;
+    elif randomNum == 4:
+      self.tileType = "road";
+      self.tileValue = -1;
+    return self.tileType;
+
+def mapSpace(length: int, width: int) -> int:
+  map = [];
+  for i in range(length):
+    map.append([]);
+    for j in range(width):
+          map[i].append(Tile.getTileType(Tile("")));
+     
+  return map;
+
+def main():
+  length = int(input("Enter the length of the map: "));
+  width = int(input("Enter the width of the map: "));
+  mapGen = mapSpace(length, width);
+  print(mapGen);
+
+if __name__ == "__main__":
+    main();
--- README.md
+++
@@ -1,2 +0,0 @@
-# Map-generation-and-Wandering-Sales-Man
-Generate a Grid map and populate it with tiles that have various tiles of various movement values, then have a salesman path through it as optimally as possible
